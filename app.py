c92e68bfbf7ba3b50f33e54db588a71b29a175cb
C3M3Z4 <cmeza@mezadigital.com>
2021-02-26 00:23:15 -0600

    [HOTFIX]API consuming pending

--- app.py
+++ app.py
@@ -1,35 +1,23 @@
 
 # include the flask library
 from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_mysqldb import MySQL
+from flask_restful import Resource, Api
+# from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
-
-# MySQL Connection
-# app.config['MYSQL_HOST'] = '192.168.64.3'
-# app.config['MYSQL_USER'] = 'cmeza'
-# app.config['MYSQL_PASSWORD'] = '4nonimouS'
-# app.config['MYSQL_DB'] = 'SportCenter'
-# app.config['MYSQL_HOST'] = 'us-cdbr-east-03.cleardb.com'
-# app.config['MYSQL_HOST'] = '0.0.0.0'
-app.config['MYSQL_HOST'] = 'mx46.hostgator.mx'
-app.config['MYSQL_USER'] = 'mezadigi_dbtest'
-app.config['MYSQL_PASSWORD'] = '4nonimouS'
-app.config['MYSQL_DB'] = 'mezadigi_sportcenter'
-# app.config['MYSQL_DB'] = 'heroku_3d1434870247f00'
-mysql = MySQL(app)
+# CORS(app)
 
 
 # Mysql Settings
 
-app.secret_key = 'mysecretkey'
-
 
 @app.route("/")
 def index():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM articles')
-    data = cur.fetchall()
+    url = 'https://api-sportcenter.herokuapp.com/articles'
+    res = requests.get(url)
+    data = res.json()
+    print(data)
     return render_template('index.html', articles=data)
 
 
@@ -40,46 +28,97 @@ def add_article():
         description = request.form['description']
         price = request.form['price']
         stock = request.form['stock']
-        cur = mysql.connection.cursor()
-        cur.execute(
-            'INSERT INTO articles (article, description, price, stock) VALUES (%s, %s, %s, %s)', (article, description, price, stock))
-        mysql.connection.commit()
+        payload = {'article': article, 'description': description,
+                   'price': price, 'stock': stock}
+        url = 'https://api-sportcenter.herokuapp.com/add'
+        res = requests.post(url, data=payload)
+        print(res)
+        # return render_template('index.html', articles=data)
+        # cur = mysql.connection.cursor()
+        # cur.execute(
+        #     'INSERT INTO articles (article, description, price, stock) VALUES (%s, %s, %s, %s)', (article, description, price, stock))
+        # mysql.connection.commit()
         flash('Article Added Succesfully')
         return redirect(url_for('index'))
 
 
-@app.route("/edit/<sku>")
+# @app.route("/add", methods=['POST'])
+# def add_article():
+#     if request.method == 'POST':
+#         article = request.form['article']
+#         description = request.form['description']
+#         price = request.form['price']
+#         stock = request.form['stock']
+
+#         url = ' https://api-sportcenter.herokuapp.com/articles'
+#         res = requests.get(url)
+#         data = res.json()
+#         print(data)
+#         return render_template('index.html', articles=data)
+
+#         cur = mysql.connection.cursor()
+#         cur.execute(
+#             'INSERT INTO articles (article, description, price, stock) VALUES (%s, %s, %s, %s)', (article, description, price, stock))
+#         mysql.connection.commit()
+#         flash('Article Added Succesfully')
+#         return redirect(url_for('index'))
+
+
+@app.route("/articles/<sku>")
 def edit_article(sku):
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM articles WHERE sku = %s', (sku))
-    data = cur.fetchall()
-    return render_template('edit_article.html', article=data[0])
+    url = ' https://api-sportcenter.herokuapp.com/articles/<sku>'
+    res = requests.get(url)
+    data = res
+    print(data)
+    return render_template('edit_article.html', article=data)
 
 
-@app.route('/update/<sku>', methods=['POST'])
-def update_article(sku):
-    if request.method == 'POST':
-        article = request.form['article']
-        description = request.form['description']
-        price = request.form['price']
-        stock = request.form['stock']
+# @app.route("/add", methods=['POST'])
+# def add_article():
+#     if request.method == 'POST':
+#         article = request.form['article']
+#         description = request.form['description']
+#         price = request.form['price']
+#         stock = request.form['stock']
+#         cur = mysql.connection.cursor()
+#         cur.execute(
+#             'INSERT INTO articles (article, description, price, stock) VALUES (%s, %s, %s, %s)', (article, description, price, stock))
+#         mysql.connection.commit()
+#         flash('Article Added Succesfully')
+#         return redirect(url_for('index'))
 
-        cur = mysql.connection.cursor()
-        cur.execute("UPDATE articles SET article = %s, description = %s, price = %s, stock = %s WHERE sku = %s ",
-                    (article, description, price, stock, sku))
-        flash('Contact Updated Successfully')
-        mysql.connection.commit()
-        return redirect(url_for('index'))
+
+# @app.route("/edit/<sku>")
+# def edit_article(sku):
+#     cur = mysql.connection.cursor()
+#     cur.execute('SELECT * FROM articles WHERE sku = %s', (sku))
+#     data = cur.fetchall()
+#     return render_template('edit_article.html', article=data[0])
 
 
-@app.route("/delete/<string:sku>")
-def delete_article(sku):
-    cur = mysql.connection.cursor()
-    cur.execute('DELETE FROM articles WHERE sku = {0}'.format(sku))
-    mysql.connection.commit()
-    flash('Article Deleted Succesfully')
-    return redirect(url_for('index'))
+# @app.route('/update/<sku>', methods=['POST'])
+# def update_article(sku):
+#     if request.method == 'POST':
+#         article = request.form['article']
+#         description = request.form['description']
+#         price = request.form['price']
+#         stock = request.form['stock']
 
+#         cur = mysql.connection.cursor()
+#         cur.execute("UPDATE articles SET article = %s, description = %s, price = %s, stock = %s WHERE sku = %s ",
+#                     (article, description, price, stock, sku))
+#         flash('Contact Updated Successfully')
+#         mysql.connection.commit()
+#         return redirect(url_for('index'))
+
+
+# @app.route("/delete/<string:sku>")
+# def delete_article(sku):
+#     cur = mysql.connection.cursor()
+#     cur.execute('DELETE FROM articles WHERE sku = {0}'.format(sku))
+#     mysql.connection.commit()
+#     flash('Article Deleted Succesfully')
+#     return redirect(url_for('index'))
 
 if __name__ == '__main__':
     # application will start listening for web request on port 5000
